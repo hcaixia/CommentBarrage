16ce4ac974219d0616f423757c1225435d34d119
Denice <Caixia@huangcaixiadeMacBook-Pro.local>
2016-08-26 17:38:28 +0800

    first
    first

---
+++ Barrage/BarrageModel.m
@@ -0,0 +1,18 @@
+//
+//  BarrageModel.m
+//  CommentBarrage
+//
+//  Created by 黄彩霞 on 16/7/1.
+//  Copyright © 2016年 HuangCaixia. All rights reserved.
+//
+
+#import "BarrageModel.h"
+
+@implementation BarrageModel
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+{
+    
+}
+
+@end
